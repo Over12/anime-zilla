0179ae4f6c392fb9685f00ad59afa25ec918284f
OverCode <avalosfigueroapedro@gmail.com>
2025-07-06 19:38:38 -0500

    Actualizar el componente Hero para utilizar datos únicos de anime y mejorar estilos visuales

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,13 @@
 import data from '@/data/sample-anime.json'
+import { getUniqueAnimeData } from '@/lib/utils'
 
 export default function Hero() {
-  const animes = data.data
+  const animes = getUniqueAnimeData(data.data)
 
   return (
     <div className="w-screen h-screen px-20 flex items-center justify-center gap-5">
       <div className="w-full">
-        <h1 className="text-5xl font-bold text-primary mb-4">Bienvenido a AnimeZilla</h1>
+        <h1 className="text-5xl font-bold text-accent mb-4">Bienvenido a AnimeZilla</h1>
         <p className="text-lg text-text mb-8">
           Tu catálogo de animes y mangas favoritos, donde puedes explorar, descubrir y disfrutar de todo lo relacionado con el mundo del anime.
         </p>
@@ -18,12 +19,14 @@ export default function Hero() {
             key={anime.mal_id}
             src={anime.images.webp.large_image_url}
             alt={anime.title}
+            title={anime.title}
+            loading="lazy"
             style={{
               transform: `translateX(${index * 32}px) scale(${1 - index * 0.05})`,
               zIndex: 5 - index,
               opacity: 1 - index * 0.05
             }}
-            className="absolute h-4/6 border-2 border-primary rounded-lg shadow-lg transition-all duration-300 hover:scale-105"
+            className="absolute h-4/6 border-4 border-accent rounded-lg shadow-lg transition-all duration-300 hover:scale-105"
           />
         ))}
       </div>
