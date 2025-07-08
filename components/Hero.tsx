ea1377b600d3909752261a2d13c4021dcbfcdab3
OverCode <avalosfigueroapedro@gmail.com>
2025-07-08 17:17:15 -0500

    Actualizar el componente Hero para mejorar la presentación y la estructura visual

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,22 +6,22 @@ export default function Hero() {
   const animes = getUniqueAnimeData(data.data)
 
   return (
-    <section className='w-full h-svh px-20'>
-      <div className='absolute bg-background/85 top-0 bottom-0 left-0 right-0 flex flex-col items-center justify-center text-center'>
-        <h1 className='text-5xl font-bold text-accent'>Bienvenido a AnimeZilla!</h1>
-        <p className='mt-3 text-lg font-semibold' >Tu catálogo de anime y manga favorito.</p>
-      </div>
-      <div className='absolute top-0 bottom-0 left-0 right-0 -z-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 p-5 overflow-hidden'>
+    <section className='relative w-svw h-svh overflow-hidden'>
+      <div className='hidden size-full place-items-start place-content-center-safe sm:grid grid-cols-3 lg:grid-cols-4 px-5 gap-5'>
         {animes.map((anime, index) => (
           <Image
             key={anime.mal_id}
             src={anime.images.webp.large_image_url}
             alt={anime.title}
-            width={500}
-            height={750}
-            className={`object-cover rounded-lg ${index % 2 === 1 ? "md:-translate-y-60 lg:translate-0 xl:-translate-y-60" : ""}`}
+            width={300}
+            height={450}
+            className={`object-cover rounded-xl w-full ${index % 2 === 1 ? "lg:translate-y-24" : "lg:translate-y-7 xl:-translate-y-24"}`}
           />
         ))}
+      </div>
+      <div className='absolute inset-0 flex flex-col items-center justify-center bg-gradient-to-b from-background from-15% to-black/70 z-10 p-7 sm:p-10 md:p-14 lg:p-20'>
+        <h1 className='text-2xl sm:text-4xl font-bold text-accent'>Bienvenido a AnimeZilla!</h1>
+        <p className='text-base text-center sm:text-lg text-text'>Descubre y explora tus animes y mangas favoritos</p>
       </div>
     </section>
   )
