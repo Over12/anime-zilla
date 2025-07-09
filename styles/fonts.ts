6d4fe73b24ec90ce4b39b3ba71b874a42c5a7011
OverCode <avalosfigueroapedro@gmail.com>
2025-07-09 14:53:14 -0500

    Agregar fuentes adicionales: Permanent Marker y Trade Winds, para mejorar la tipografía del proyecto

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -1,7 +1,19 @@
-import { Onest } from 'next/font/google'
+import { Onest, Permanent_Marker, Trade_Winds } from 'next/font/google'
 
 export const onest = Onest({
   subsets: ['latin'],
   display: 'swap',
   weight: ['400', '500', '600', '700']
+})
+
+export const permanentMarker = Permanent_Marker({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400'
+})
+
+export const tradeWinds = Trade_Winds({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400'
 })
