4c3f7a66adac006634137dd72d7ad4151a549e5d
OverCode <avalosfigueroapedro@gmail.com>
2025-07-09 20:53:58 -0500

    Agregar estructura para los botones de navegación en el componente Carrousel

--- components/Carrousel.tsx
+++ components/Carrousel.tsx
@@ -4,8 +4,10 @@ import { permanentMarker } from "@/styles/fonts";
 import { Anime } from "@/types/anime";
 
 export default function Carrousel({ title, animeData }: { title: string, animeData: Anime[] }) {
+  //TODO: Implementar la lógica para los botones de navegación del carrousel
+
   return (
-    <>
+    <div className="relative">
       <h2 className={`${permanentMarker.className} text-2xl sm:text-3xl text-primary font-bold`}>{title}</h2>
       <div className="flex overflow-x-auto gap-5 py-5 snap-x hide-scrollbar">
         {animeData.map((anime) => (
@@ -20,7 +22,13 @@ export default function Carrousel({ title, animeData }: { title: string, animeDa
             <h3 className="font-semibold text-sm sm:text-base mt-2 w-40 sm:w-52 lg:w-64 line-clamp-1 group-hover:text-primary transition-colors">{anime.title}</h3>
           </Link>
         ))}
+        <button className="absolute top-1/2 -translate-1/2 left-0 bg-accent rounded-full p-2 size-10 hidden sm:block">
+          <span className="text-sm sm:text-base">&lt;</span>
+        </button>
+        <button className="absolute top-1/2 -translate-y-1/2 translate-x-1/2 right-0 bg-accent rounded-full p-2 size-10 hidden sm:block">
+          <span className="text-sm sm:text-base">&gt;</span>
+        </button>
       </div>
-    </>
+    </div>
   )
 }
