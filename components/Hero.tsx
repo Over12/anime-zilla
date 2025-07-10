3fb87b49087561ef1e429f999115d08545bc5fc7
OverCode <avalosfigueroapedro@gmail.com>
2025-07-09 20:41:43 -0500

    Actualizar el componente Hero para mejorar la presentación de los animes y ajustar estilos de texto

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,6 +6,9 @@ import { permanentMarker } from '@/styles/fonts'
 export default function Hero() {
   const animes = getUniqueAnimeData(data.data)
 
+  //* Randomize the order of the animes
+  animes.sort(() => Math.random() - 0.5)
+
   return (
     <section className='relative w-full h-svh overflow-hidden'>
       <div className='hidden size-full place-items-start place-content-center-safe sm:grid grid-cols-3 lg:grid-cols-4 px-5 gap-5'>
@@ -16,13 +19,13 @@ export default function Hero() {
             alt={anime.title}
             width={300}
             height={450}
-            className={`object-cover rounded-xl w-full ${index % 2 === 1 ? "lg:translate-y-24" : "lg:translate-y-7 xl:-translate-y-24"}`}
+            className={`object-fill rounded-xl size-full ${index % 2 === 1 ? "lg:translate-y-24" : "lg:translate-y-7 xl:-translate-y-24"}`}
           />
         ))}
       </div>
       <div className='absolute inset-0 flex flex-col items-center justify-center bg-gradient-to-b from-background from-15% to-black/70 z-10 p-7 sm:p-10 md:p-14 lg:p-20'>
-        <h1 className={`${permanentMarker.className} text-2xl sm:text-4xl font-bold text-accent`}>Bienvenido a AnimeZilla!</h1>
-        <p className='text-center text-sm sm:text-base md:text-lg text-text/80'>Descubre y explora tus animes y mangas favoritos</p>
+        <h1 className={`${permanentMarker.className} text-2xl sm:text-4xl lg:text-5xl font-bold text-accent`}>Bienvenido a AnimeZilla!</h1>
+        <p className='text-center text-sm sm:text-base md:text-lg text-text/70'>Descubre y explora tus animes y mangas favoritos</p>
       </div>
     </section>
   )
