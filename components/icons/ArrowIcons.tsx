f4d019486b0571ac28e8e9bd27dd96a06f42ce0d
OverCode <avalosfigueroapedro@gmail.com>
2025-07-11 17:20:54 -0500

    Agregar iconos de flecha izquierda y derecha como componentes SVG

---
+++ components/icons/ArrowIcons.tsx
@@ -0,0 +1,11 @@
+export function ArrowLeftIcon({ className = "" }: { className?: string }) {
+  return (
+    <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="currentColor"  className={`w-6 h-6 ${className}`}><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9.586 4l-6.586 6.586a2 2 0 0 0 0 2.828l6.586 6.586a2 2 0 0 0 2.18 .434l.145 -.068a2 2 0 0 0 1.089 -1.78v-2.586h7a2 2 0 0 0 2 -2v-4l-.005 -.15a2 2 0 0 0 -1.995 -1.85l-7 -.001v-2.585a2 2 0 0 0 -3.414 -1.414z" /></svg>
+  )
+}
+
+export function ArrowRightIcon({ className = "" }: { className?: string }) {
+  return (
+    <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="currentColor"  className={`w-6 h-6 ${className}`}><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12.089 3.634a2 2 0 0 0 -1.089 1.78l-.001 2.586h-6.999a2 2 0 0 0 -2 2v4l.005 .15a2 2 0 0 0 1.995 1.85l6.999 -.001l.001 2.587a2 2 0 0 0 3.414 1.414l6.586 -6.586a2 2 0 0 0 0 -2.828l-6.586 -6.586a2 2 0 0 0 -2.18 -.434l-.145 .068z" /></svg>
+  )
+}
