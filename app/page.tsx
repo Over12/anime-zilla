d6c51a308b699c1db1f0d9aee9cd4045ea73b1fc
OverCode <avalosfigueroapedro@gmail.com>
2025-07-11 18:57:58 -0500

    Actualizar la función Home para obtener animes de la API en lugar de datos de muestra

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 //* Este es la pagina principal de la aplicación
 
-import Carrousel from "@/components/Carrousel";
-import Hero from "@/components/Hero";
-import AnimeData from "@/data/sample-anime.json"
-import NavBar from "@/components/NavBar";
-import { getUniqueAnimeData } from "@/lib/utils";
+import Carrousel from "@/components/Carrousel"
+import Hero from "@/components/Hero"
+import NavBar from "@/components/NavBar"
+import { getAnimeSeason } from "@/lib/api"
 
-export default function Home() {
-  const animes = getUniqueAnimeData(AnimeData.data);
+export default async function Home() {
+  const animes = await getAnimeSeason()
 
   return (
     <>
