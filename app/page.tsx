d790bfb9fab1dee588ad9eaf5d472aa3d265e038
OverCode <avalosfigueroapedro@gmail.com>
2025-07-14 17:17:25 -0500

    Actualizar la página principal para incluir el componente Carrousel con datos de mangas y animes

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,21 @@
 import Carrousel from "@/components/Carrousel"
 import Hero from "@/components/Hero"
 import NavBar from "@/components/NavBar"
-import { getAnimeSeason } from "@/lib/api"
+import { getAnimeSeason, getTopAnimes, getTopMangas } from "@/lib/api"
 
 export default async function Home() {
-  const animes = await getAnimeSeason()
+  const season = await getAnimeSeason()
+  const animes = await getTopAnimes()
+  const mangas = await getTopMangas()
 
   return (
     <>
       <NavBar />
-      <Hero />
+      <Hero season={season} />
       <section className="py-10 px-7 sm:px-10 md:px-14 lg:px-20">
-        <Carrousel title="Últimos animes" animeData={animes} />
-        <Carrousel title="Top animes" animeData={animes} />
-        <Carrousel title="Top mangas" animeData={animes} />
+        <Carrousel title="Últimos animes" data={season} />
+        <Carrousel title="Top animes" data={animes} />
+        <Carrousel title="Top mangas" data={mangas} />
       </section>
     </>
   );
