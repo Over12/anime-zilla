a166dfb82b9a37aafcf74a0b9c64e79e85c4ba0a
OverCode <avalosfigueroapedro@gmail.com>
2025-07-15 16:49:16 -0500

    Actualizar el componente Carrousel para mejorar la disposición visual y la accesibilidad de los botones de navegación

--- components/Carrousel.tsx
+++ components/Carrousel.tsx
@@ -37,8 +37,8 @@ export default function Carrousel({ title, data }: { title: string, data: (Anime
 
   return (
     <div className="relative">
-      <h2 className={`${permanentMarker.className} text-2xl sm:text-3xl text-primary font-bold`}>{title}</h2>
-      <div ref={carrouselRef} className="flex overflow-x-auto gap-5 py-5 snap-x hide-scrollbar">
+      <h2 className={`${permanentMarker.className} text-2xl sm:text-4xl text-primary font-bold`}>{title}</h2>
+      <div ref={carrouselRef} className="flex overflow-x-auto gap-4 py-5 snap-x hide-scrollbar">
         {data.map((anime) => (
           <Link key={anime.mal_id} href={`/anime/${anime.mal_id}`} className="flex-shrink-0 aspect-[2/3] group">
             <Image
@@ -46,17 +46,17 @@ export default function Carrousel({ title, data }: { title: string, data: (Anime
               alt={anime.title}
               width={300}
               height={450}
-              className="object-fill rounded-xl h-full w-40 sm:w-52 lg:w-64 snap-center sm:snap-start"
+              className="object-fill rounded-xl sm:transition-opacity sm:opacity-70 sm:group-hover:opacity-100 h-full w-40 sm:w-52 lg:w-64 snap-center sm:snap-start"
             />
-            <h3 className="font-semibold text-sm sm:text-base mt-2 w-40 sm:w-52 lg:w-64 line-clamp-1 group-hover:text-primary transition-colors">{anime.title}</h3>
+            <h3 className="font-semibold text-sm sm:text-base mt-2 w-40 sm:w-52 lg:w-64 line-clamp-1 group-hover:text-primary transition-colors" title={anime.title}>{anime.title}</h3>
           </Link>
         ))}
-        <div className="absolute top-0 right-0 flex items-start gap-5">
-          <button onClick={onLeftClick} className="cursor-pointer hover:scale-110 transition-transform bg-secondary/70 backdrop-blur-lg border-2 border-accent rounded-full p-2 hidden sm:block">
-            <ArrowLeftIcon className="text-text" />
+        <div className="hidden sm:flex absolute top-0 right-0 gap-3">
+          <button onClick={onLeftClick} className="cursor-pointer p-1 transition-opacity opacity-70 hover:opacity-100 border-4 border-primary rounded-full">
+            <ArrowLeftIcon className="text-primary size-5" />
           </button>
-          <button onClick={onRightClick} className="cursor-pointer hover:scale-110 transition-transform bg-secondary/70 backdrop-blur-lg border-2 border-accent rounded-full p-2 hidden sm:block">
-            <ArrowRightIcon className="text-text" />
+          <button onClick={onRightClick} className="cursor-pointer p-1 transition-opacity opacity-70 hover:opacity-100 border-4 border-primary rounded-full">
+            <ArrowRightIcon className="text-primary size-5" />
           </button>
         </div>
       </div>
