69f716d2f5685eefb49f496157e05892c0efb75a
OverCode <avalosfigueroapedro@gmail.com>
2025-07-16 21:04:08 -0500

    Agregar comentarios descriptivos a las funciones de la API para mejorar la legibilidad del código

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,7 @@
+import { Anime } from "@/types/anime"
 import { getUniqueAnimeData } from "./utils"
 
+//* Obtener la lista de los 15 primeros animes de la temporada actual
 export async function getAnimeSeason() {
   const response = await fetch(`${process.env.API_URL}/seasons/now?limit=15`, {
     next: { revalidate: 300 }
@@ -11,6 +13,7 @@ export async function getAnimeSeason() {
   return getUniqueAnimeData(data.data)
 }
 
+//* Obtener la lista de los 15 primeros animes populares
 export async function getTopAnimes() {
   const response = await fetch(`${process.env.API_URL}/top/anime?limit=15`, {
     next: { revalidate: 300 }
@@ -22,6 +25,7 @@ export async function getTopAnimes() {
   return getUniqueAnimeData(data.data)
 }
 
+//* Obtener la lista de los 15 primeros mangas populares
 export async function getTopMangas() {
   const response = await fetch(`${process.env.API_URL}/top/manga?limit=15`, {
     next: { revalidate: 300 }
@@ -31,4 +35,16 @@ export async function getTopMangas() {
   const data = await response.json()
 
   return getUniqueAnimeData(data.data)
+}
+
+//* Obtener información de un anime específico por ID
+export async function getAnimeById(id: number): Promise<Anime> {
+  const response = await fetch(`${process.env.API_URL}/anime/${id}/full`, {
+    next: { revalidate: 300 }
+  })
+
+  if (!response.ok) throw new Error("Error al obtener el anime")
+  const data = await response.json()
+
+  return data.data
 }
