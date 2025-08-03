5c65fefb3ecb65aa609124c758076be5da7b9e7b
OverCode <avalosfigueroapedro@gmail.com>
2025-08-03 18:57:27 -0500

    Agregar componente MediaFilter para mejorar la filtración y búsqueda de medios en el contenedor de medios.

--- components/media/MediaContainer.tsx
+++ components/media/MediaContainer.tsx
@@ -3,11 +3,13 @@ import { ApiResponse } from "@/types/apiResponse";
 import { Manga } from "@/types/manga";
 import MediaCard from "./MediaCard";
 import MediaContainerSkeleton from "../ui/MediaContainerSkeleton";
+import MediaFilter from "./MediaFilter";
 
 export default function MediaContainer({ title = "Media", media, loading = false }: { title?: string, media: ApiResponse<Anime | Manga> | null, loading?: boolean }) {
   return (
     <main className="px-7 pt-24 sm:px-10 md:px-14 lg:px-20">
       <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold">{title}</h1>
+      <MediaFilter media_type={title.toLowerCase()} />
 
       {loading ? (
         <MediaContainerSkeleton />
