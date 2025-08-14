f3d2c4e3320db3e12f0286d6be2ecd62f73f6037
OverCode <avalosfigueroapedro@gmail.com>
2025-08-14 18:35:09 -0500

    Mejorar el diseño y la estructura del formulario en MediaFilter; ajustar estilos y disposición de elementos para una mejor usabilidad.

--- app/animes/page.tsx
+++ app/animes/page.tsx
@@ -9,17 +9,17 @@ export default function AnimesPage() {
   return (
     <>
       <MediaContainer title="Animes" media={media} loading={loading} updater={updateFilter} />
-      <div className="flex justify-center items-center mt-10">
-        <button onClick={previousPage} className="cursor-pointer text-center px-4 py-2 bg-secondary hover:bg-accent font-semibold rounded">
+      <div className="flex justify-center items-center mt-3 sm:mt-5 lg:mt-10">
+        <button onClick={previousPage} className="cursor-pointer text-center text-sm px-2 py-2 sm:text-base sm:px-4 bg-secondary hover:bg-accent font-semibold rounded">
           Anterior
         </button>
         
-        <span className="font-semibold mx-5 text-sm text-center">
+        <span className="font-semibold mx-3 sm:mx-5 text-sm text-center">
           Página <input type="text" value={selectedPage.toString()} onKeyUp={onKeyPressInput} onChange={changePage} onBlur={onBlurInput} placeholder="1" className="w-8 h-6 text-center border-b-2 border-accent bg-transparent focus:outline-none" />
           <span> de {media?.pagination?.lastPage}</span>
         </span>
 
-        <button onClick={nextPage} className="cursor-pointer text-center px-4 py-2 bg-secondary hover:bg-accent font-semibold rounded">
+        <button onClick={nextPage} className="cursor-pointer text-center text-sm px-2 py-2 sm:text-base sm:px-4 bg-secondary hover:bg-accent font-semibold rounded">
           Siguiente
         </button>
       </div>
--- components/media/MediaFilter.tsx
+++ components/media/MediaFilter.tsx
@@ -23,61 +23,65 @@ export default function MediaFilter({ media_type, updater }: { media_type: strin
   }
 
   return (
-    <form className="flex gap-5 mt-3" onSubmit={onSubmit}>
-      <div className="flex gap-2 justify-center items-center h-full">
-        <label htmlFor="media_query" className="text-sm font-semibold">Search:</label>
-        <input type="text" id="media_query" name="q" placeholder="Danshi Kōkōsei no Nichijō..." onChange={handleInputChange} className="px-2 py-1 border-b-2 border-accent size-full focus:outline-none" />
-      </div>
-      <div className="flex gap-2 justify-center items-center h-full">
-        <label htmlFor="media_type" className="text-sm font-semibold">Type:</label>
-        <select id="media_type" name="type" onChange={handleInputChange} className="bg-background px-2 py-1 border-b-2 border-accent cursor-pointer focus:outline-none">
-          {media_type === "animes" && (
-            <>
-              <option value="">All Types</option>
-              <option value="tv">TV</option>
-              <option value="movie">Movie</option>
-              <option value="ova">OVA</option>
-              <option value="special">Special</option>
-              <option value="ona">ONA</option>
-              <option value="music">Music</option>
-              <option value="cm">CM</option>
-              <option value="pv">PV</option>
-              <option value="tv_special">TV Special</option>
-            </>
-          )}
-          {media_type === "mangas" && (
-            <>
-              <option value="">All Types</option>
-              <option value="manga">Manga</option>
-              <option value="novel">Novel</option>
-              <option value="lightnovel">Light Novel</option>
-              <option value="oneshot">One-Shot</option>
-              <option value="doujin">Doujin</option>
-              <option value="manhwa">Manhwa</option>
-              <option value="manhua">Manhua</option>
-            </>
-          )}
-        </select>
-      </div>
-      {media_type === "animes" && (
+    <form className="flex flex-col lg:flex-row gap-3 sm:gap-5 mt-3 sm:mt-4" onSubmit={onSubmit}>
+      <div className="flex flex-col items-start justify-center sm:flex-row sm:justify-around gap-3 sm:gap-5">
         <div className="flex gap-2 justify-center items-center h-full">
-          <label htmlFor="media_rating" className="text-sm font-semibold">Rating:</label>
-          <select id="media_rating" name="rating" onChange={handleInputChange} className="bg-background px-2 py-1 border-b-2 border-accent cursor-pointer focus:outline-none">
-            <option value="">All Ratings</option>
-            <option value="g">G - All Ages</option>
-            <option value="pg">PG - Children</option>
-            <option value="pg13">PG-13 - Teens 13 or older</option>
-            <option value="r17">R - 17+ (Violence & profanity)</option>
-            <option value="r">R+ - Mild Nudity</option>
-            <option value="rx">Rx - Hentai</option>
+          <label htmlFor="media_query" className="text-sm font-semibold">Search:</label>
+          <input type="text" id="media_query" name="q" placeholder="Danshi Kōkōsei no Nichijō..." onChange={handleInputChange} className="px-2 py-1 border-b-2 border-accent size-full focus:outline-none" />
+        </div>
+        <div className="flex gap-2 justify-center items-center h-full">
+          <label htmlFor="media_type" className="text-sm font-semibold">Type:</label>
+          <select id="media_type" name="type" onChange={handleInputChange} className="bg-background px-2 py-1 border-b-2 border-accent cursor-pointer focus:outline-none">
+            {media_type === "animes" && (
+              <>
+                <option value="">All Types</option>
+                <option value="tv">TV</option>
+                <option value="movie">Movie</option>
+                <option value="ova">OVA</option>
+                <option value="special">Special</option>
+                <option value="ona">ONA</option>
+                <option value="music">Music</option>
+                <option value="cm">CM</option>
+                <option value="pv">PV</option>
+                <option value="tv_special">TV Special</option>
+              </>
+            )}
+            {media_type === "mangas" && (
+              <>
+                <option value="">All Types</option>
+                <option value="manga">Manga</option>
+                <option value="novel">Novel</option>
+                <option value="lightnovel">Light Novel</option>
+                <option value="oneshot">One-Shot</option>
+                <option value="doujin">Doujin</option>
+                <option value="manhwa">Manhwa</option>
+                <option value="manhua">Manhua</option>
+              </>
+            )}
           </select>
         </div>
-      )}
-      <div className="flex gap-2 justify-center items-center">
-        <label htmlFor="media_sfw" className="text-sm font-semibold">Safe For Work:</label>
-        <input type="checkbox" name="sfw" id="media_sfw" onChange={handleInputChange} checked={filter.sfw} className="size-4 cursor-pointer checked:accent-accent outline-none" />
       </div>
-      <button type="submit" className="font-semibold rounded-lg bg-accent py-1 px-3 cursor-pointer hover:bg-secondary transition-colors">Search</button>
+      <div className="flex flex-col items-start h-full sm:flex-row sm:justify-around sm:items-center gap-3 sm:gap-5">
+        {media_type === "animes" && (
+          <div className="flex gap-2 justify-center items-center h-full">
+            <label htmlFor="media_rating" className="text-sm font-semibold">Rating:</label>
+            <select id="media_rating" name="rating" onChange={handleInputChange} className="bg-background px-2 py-1 border-b-2 border-accent cursor-pointer focus:outline-none">
+              <option value="">All Ratings</option>
+              <option value="g">G - All Ages</option>
+              <option value="pg">PG - Children</option>
+              <option value="pg13">PG-13 - Teens 13 or older</option>
+              <option value="r17">R - 17+ (Violence & profanity)</option>
+              <option value="r">R+ - Mild Nudity</option>
+              <option value="rx">Rx - Hentai</option>
+            </select>
+          </div>
+        )}
+        <div className="flex gap-2 justify-center items-center">
+          <label htmlFor="media_sfw" className="text-sm font-semibold lg:py-1">Safe For Work:</label>
+          <input type="checkbox" name="sfw" id="media_sfw" onChange={handleInputChange} checked={filter.sfw} className="size-4 cursor-pointer checked:accent-accent outline-none" />
+        </div>
+      </div>
+      <button type="submit" className="font-semibold w-full sm:w-auto rounded-lg bg-accent py-1 px-3 cursor-pointer hover:bg-secondary transition-colors">Search</button>
     </form>
   )
 }
