5a4acf2f2774ec1f568c62c30acd483bb68bcc27
OverCode <avalosfigueroapedro@gmail.com>
2025-08-14 19:35:14 -0500

    Traducir los textos de navegación en las páginas de Animes y Mangas a inglés para mantener la coherencia en la interfaz de usuario.

--- app/mangas/page.tsx
+++ app/mangas/page.tsx
@@ -11,16 +11,16 @@ export default function MangasPage() {
       <MediaContainer title="Mangas" media={media} loading={loading} updater={updateFilter} />
       <div className="flex justify-center items-center mt-10">
         <button onClick={previousPage} className="cursor-pointer text-center px-4 py-2 bg-secondary hover:bg-accent font-semibold rounded">
-          Anterior
+          Previous
         </button>
         
         <span className="font-semibold mx-5 text-sm text-center">
-          Página <input type="text" value={selectedPage.toString()} onKeyUp={onKeyPressInput} onChange={changePage} onBlur={onBlurInput} placeholder="1" className="w-8 h-6 text-center border-b-2 border-accent bg-transparent focus:outline-none" />
-          <span> de {media?.pagination?.lastPage}</span>
+          Page <input type="text" value={selectedPage.toString()} onKeyUp={onKeyPressInput} onChange={changePage} onBlur={onBlurInput} placeholder="1" className="w-8 h-6 text-center border-b-2 border-accent bg-transparent focus:outline-none" />
+          <span> of {media?.pagination?.lastPage}</span>
         </span>
 
         <button onClick={nextPage} className="cursor-pointer text-center px-4 py-2 bg-secondary hover:bg-accent font-semibold rounded">
-          Siguiente
+          Next
         </button>
       </div>
     </>
